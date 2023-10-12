9dcefbafb80996dcc1e2d482c70166a5c980d597
yashvi2399 <yashvi.pithadia@mail.concordia.ca>
2023-10-11 22:46:07 -0400

    Reorder Access Modifiers - line 22 (issue #17)

--- src/main/java/de/storchp/opentracks/osmplugin/utils/PreferencesUtils.java
+++ src/main/java/de/storchp/opentracks/osmplugin/utils/PreferencesUtils.java
@@ -19,7 +19,7 @@ import de.storchp.opentracks.osmplugin.R;
 public class PreferencesUtils {
 
     private final static String TAG = PreferencesUtils.class.getSimpleName();
-    private static final Set<String> DEFAULT_STATISTIC_ELEMENTS = Set.of(
+    private final static Set<String> DEFAULT_STATISTIC_ELEMENTS = Set.of(
             StatisticElement.CATEGORY.name(),
             StatisticElement.MOVING_TIME.name(),
             StatisticElement.DISTANCE_KM.name(),
