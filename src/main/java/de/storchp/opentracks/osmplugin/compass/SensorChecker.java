dff3a794e3016530e7d87c825b9913ba5e2aed74
Khyati-Bareja <117713327+Khyati-Bareja@users.noreply.github.com>
2023-10-12 10:42:57 -0400

    Added a Private constructor to hide the implicit public one

--- src/main/java/de/storchp/opentracks/osmplugin/compass/SensorChecker.java
+++ src/main/java/de/storchp/opentracks/osmplugin/compass/SensorChecker.java
@@ -10,6 +10,9 @@ import android.hardware.SensorManager;
  * Derived from <a href="https://github.com/kylecorry31/Trail-Sense/blob/master/app/src/main/java/com/kylecorry/trail_sense/shared/sensors/SensorChecker.kt">...</a>
  */
 public class SensorChecker {
+    private SensorChecker() {
+        throw new IllegalStateException("Utility class");
+    }
 
     public static boolean hasGravity(Context context) {
         var sensors = ((SensorManager) context.getSystemService(SENSOR_SERVICE)).getSensorList(Sensor.TYPE_GRAVITY);
