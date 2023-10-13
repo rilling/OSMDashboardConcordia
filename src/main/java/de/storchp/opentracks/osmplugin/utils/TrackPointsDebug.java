484f43947a5d1163a8dd4ba43eca0732348ea08d
mukul <mukulsharma1998@gmail.com>
2023-10-13 18:16:28 -0400

    removing final keyword

--- src/main/java/de/storchp/opentracks/osmplugin/utils/TrackPointsDebug.java
+++ src/main/java/de/storchp/opentracks/osmplugin/utils/TrackPointsDebug.java
@@ -1,9 +1,9 @@
 package de.storchp.opentracks.osmplugin.utils;
 
 public class TrackPointsDebug {
-    public static final int trackpointsReceived = 0;
-    public static final int trackpointsInvalid = 0;
-    public static final int trackpointsDrawn = 0;
+    public static  int trackpointsReceived = 0;
+    public static  int trackpointsInvalid = 0;
+    public static  int trackpointsDrawn = 0;
     public int trackpointsPause = 0;
     public int segments = 0;
 
